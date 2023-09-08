cc29449f7b7cd60955bb0f072577ea4285770f8d
notpritam <notpritamsharma@gmail.com>
2023-09-09 01:16:20 +0530

    Design

--- components/shared/AboutUs.tsx
+++ components/shared/AboutUs.tsx
@@ -4,8 +4,8 @@ const AboutUs = () => {
   return (
     <section className=" text-white mt-[10rem]">
       <div className="container flex">
-        <div className="flex flex-1">
-          <div className="flex flex-1 flex-col">
+        <div className="flex gap-4 flex-1">
+          <div className="flex gap-4 flex-1 flex-col">
             <div className="w-full h-full overflow-hidden">
               <Image
                 src="/assets/img/second.jpg"
@@ -22,21 +22,21 @@ const AboutUs = () => {
                 width={300}
                 src="/assets/img/blood.avif"
                 alt="Image"
-                className="w-full h-full object-cover"
+                className="w-full h-full object-fill"
               />
             </div>
           </div>
-          <div className="h-[600px] bg-red-400 flex-1 w-full">
+          <div className="h-[600px]  flex-1 w-full">
             <img
               src="https://images.unsplash.com/photo-1582719202047-76d3432ee323?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80"
               alt=""
-              className="w-full object-cover"
+              className="w-full h-full object-cover"
             />
           </div>
         </div>
 
-        <div className="w-full flex-1 flex p-4 pt-[8rem]">
-          <div className="mt-10 lg:mt-0">
+        <div className="w-full flex-1 flex p-8 pt-[4rem]">
+          <div className="">
             <span className="block mb-2 text-lg font-semibold text-primary">
               Why Donate Blood
             </span>
@@ -50,7 +50,7 @@ const AboutUs = () => {
             </p>
             <a
               href="/#"
-              className="inline-flex items-center justify-center px-10 py-4 text-base font-normal text-center text-white rounded-lg bg-primary hover:bg-opacity-90 lg:px-8 xl:px-10"
+              className="inline-flex bg-white text-black items-center justify-center px-10 py-4 text-base font-normal text-center rounded-lg bg-primary hover:bg-opacity-90 lg:px-8 xl:px-10"
             >
               Get Started
             </a>
